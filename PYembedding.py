799dd4eb4f18c49d89f2ecada972fd92dd8427ab
陈泽远 <76687230+chen-ze-yuan@users.noreply.github.com>
2025-06-07 15:57:11 +0800

    Add files via upload
    Signed-off-by: 陈泽远 <76687230+chen-ze-yuan@users.noreply.github.com>

--- PYembedding.py
+++ PYembedding.py
@@ -19,22 +19,11 @@ class PinyinEmbeddingCNN(nn.Module):
         x = x.view(B, T, -1)  # (B, T, cnn_out_dim)
         return x  # yemb_i
 
-class PinyinFusionModule(nn.Module):
-    def __init__(self, encoder_dim=512, pinyin_dim=128, out_dim=512):
-        super().__init__()
-        self.fc = nn.Linear(encoder_dim + pinyin_dim, out_dim)
 
-    def forward(self, h, yemb):  # h: (B, T, 512), yemb: (B, T, 128)
-        hconcat = torch.cat([h, yemb], dim=-1)  # (B, T, 640)
-        hchar = self.fc(hconcat)  # (B, T, 512)
-        return hchar
 
 
 def pad_pinyin(pinyin_strs, pad_len=8, pad_char='-'):
     # pinyin_strs: list of strings, e.g. ['zhuang', 'guo']
     return [list(p[:pad_len].ljust(pad_len, pad_char)) for p in pinyin_strs]
 
-def encode_pinyin_characters(padded_pinyin_list, char2id):
-    # padded_pinyin_list: list of list of chars
-    # char2id: dict mapping char to int
-    return torch.tensor([[char2id.get(ch, char2id['-']) for ch in syllable] for syllable in padded_pinyin_list])
+
